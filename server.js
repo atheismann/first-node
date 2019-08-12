d9dd1acda638a7914df72cd9fbd302beba9807ee
Andrew Theismann <andrew@theismannconsulting.com>
2019-08-12 16:30:25 -0500

    update

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 // load express module
 const express = require('express');
+const path = require('path');
+
+//require the todo "database"
+const todoDb = require('./data/todo-db');
 
 // create our express app
 const app = express();
 
+//configure the app (app.set)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//mount middleware (app.use)
+
+//mount routes
 //define a "root" route directly on app
 //Tomorrow, we'll use best practice routing
-app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'));
+app.get('/', (req, res) => res.redirect('/home'));
+app.get('/home', (req, res) => res.render('home', {cohort: 'SEI-CC-4'}));
+app.get('/todos', (req, res) => res.render('todos/index', {todos: todoDb.getAll()}));
 
 // tell the app to listed on port 3000
 // for HTTP requests from clients
 const port = 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
